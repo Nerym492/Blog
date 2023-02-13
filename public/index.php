b486a26142d777ef525aafc0bf77e2d3deb7eac9
Florian Pohu <florianpohu49@gmail.com>
2023-02-13 10:58:26 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../Templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__ . '/tmp',
+]);
+
+echo $twig->render('base.html.twig');
