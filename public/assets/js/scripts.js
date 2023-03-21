953868ee8fdee52aaed80645959b06bb62ec6279
Florian Pohu <florianpohu49@gmail.com>
2023-03-21 09:20:04 +0100

    Bug fix -> help messages after form submission

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,13 +1,14 @@
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input[type="text"], input[type="checkbox"], textarea');
 
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
 let forms = document.querySelectorAll('.needs-validation');
-let mailRegEx = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+const mailRegEx = /^([A-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+const fullNameRegEx = /^([A-z]){3,25}\s([A-z]){3,25}$/
 
 let password = document.getElementById("password-register");
 let groupPassword = document.getElementById("group-password-register");
 let passwordAlert = document.getElementById("password-alert");
-let passwordInfoMessage = document.getElementById("password-info-message");
+let passwordHelp = document.getElementById("password-register-help");
 let passwordConfirm = document.getElementById("password-register-confirm");
 
 const passwordPatterns = {
@@ -19,17 +20,20 @@ const passwordPatterns = {
 }
 
 const patterns = {
-    "full-name-contact": /^([A-z]){3,25}\s([A-z]){3,25}$/,
+    "full-name-contact": fullNameRegEx,
     "mail-contact": mailRegEx,
+    "comment-contact": /^[\s\S]+$/,
+    "pseudo-register": /^[A-z\d]{3,25}$/,
+    "full-name-register": fullNameRegEx,
     "mail-register": mailRegEx
 };
 
 //Check if the confirmation password match with the password
-function checkPasswordConfirm(password, passwordConfirm){
-    if (passwordConfirm.value === password.value){
+function checkPasswordConfirm(password, passwordConfirm) {
+    if (passwordConfirm.value === password.value && passwordConfirm.value !== "") {
         passwordConfirm.className = 'form-control valid';
         passwordConfirm.parentElement.classList.add('mb-3');
-    } else if (passwordConfirm.value !== password && passwordConfirm.value !== ""){
+    } else if (passwordConfirm.value !== password && passwordConfirm.value !== "") {
         passwordConfirm.className = 'form-control invalid';
         passwordConfirm.parentElement.classList.remove('mb-3');
     } else {
@@ -39,12 +43,13 @@ function checkPasswordConfirm(password, passwordConfirm){
 }
 
 /**Set css classes (invalid = red, valid = green)
- When the field is empty, the css classes are removed*/
-function validate(field, regex) {
-    if (field.value === "") {
+ When the field is empty, the css classes are removed
+ If the form has just been validated, errors are displayed*/
+function validate(field, regex, afterSubmit = false) {
+    if (field.value === "" && !afterSubmit) {
         field.className = 'form-control';
         field.parentElement.className = 'form-floating mb-3';
-    } else if (regex.test(field.value)) {
+    } else if (regex !== "" && regex.test(field.value)) {
         field.className = 'form-control valid';
         field.parentElement.className = 'form-floating mb-3';
     } else {
@@ -65,7 +70,6 @@ window.addEventListener('DOMContentLoaded', () => {
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
-                console.log(123);
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
@@ -83,18 +87,16 @@ window.addEventListener('DOMContentLoaded', () => {
 //When the user fills in the field, check if the expected pattern matches.
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
-        if (e.target.type !== "password") {
-            //For all other inputs
+        if (patterns[e.target.attributes.id.value] !== undefined) {
+            //If a pattern is defined, check the validity
             validate(e.target, patterns[e.target.attributes.id.value]);
         }
     });
 });
 
-if (password !== null){
+if (password !== null) {
     password.addEventListener("keyup", (e) => {
-        if (e.target.value === "") {
-            groupPassword.classList.remove("valid", "invalid");
-        } else {
+        if (e.target.value !== "") {
             passwordAlert.classList.remove('mt-3')
         }
 
@@ -116,16 +118,20 @@ if (password !== null){
         }
 
         if (nbValidPatterns === nbPatterns) {
-            passwordInfoMessage.innerHTML = "Your password is secure.";
-            passwordInfoMessage.classList.replace("invalid", "valid");
+            passwordHelp.innerHTML = "Your password is secure.";
+            passwordHelp.classList.replace("invalid", "valid");
             e.target.classList.add("valid");
             e.target.classList.remove("invalid");
-        } else {
-            passwordInfoMessage.innerHTML = "Your password does not meet all the required criteria.";
-            passwordInfoMessage.classList.replace("valid", "invalid");
+        } else if (nbValidPatterns !== nbPatterns && e.target.value !== "") {
+            passwordHelp.innerHTML = "Your password does not meet all the required criteria.";
+            passwordHelp.classList.replace("valid", "invalid");
             e.target.classList.add("invalid");
             e.target.classList.remove("valid");
+        } else {
+            password.classList.remove("valid", "invalid");
+            passwordAlert.classList.add('mt-3')
         }
+
 
         checkPasswordConfirm(e.target, passwordConfirm);
     })
@@ -133,11 +139,11 @@ if (password !== null){
     password.addEventListener("focus", () => {
         passwordAlert.classList.remove("hidden");
         passwordAlert.classList.add("visible");
-        if (password.classList.contains("valid") || password.classList.contains("invalid")){
+        if (password.classList.contains("valid") || password.classList.contains("invalid")) {
             passwordAlert.classList.remove("mt-3");
             groupPassword.classList.add("mb-3");
         } else {
-            if (!password.classList.contains("mt-3")){
+            if (!password.classList.contains("mt-3")) {
                 passwordAlert.classList.add("mt-3");
             }
         }
@@ -168,14 +174,17 @@ Array.prototype.slice.call(forms)
              and it adds the right css class*/
             if (!form.checkValidity() || document.querySelectorAll('.invalid').length > 0) {
                 inputs.forEach((input) => {
-                    if (input.value === "") {
-                        input.className = "form-control invalid";
-                        input.parentElement.className = 'form-floating';
+                    if (input.value === "" || (input.type === "checkbox" && !input.checked)) {
+                        if (input.type === "checkbox") {
+                            input.className = "form-check-input invalid me-2";
+                        } else {
+                            input.className = "form-control invalid";
+                            input.parentElement.className = 'form-floating';
+                        }
                     }
                 });
 
                 //Add css bootstrap class to the comment => display the div block if the field is not valid
-                form.classList.add('was-validated');
                 event.preventDefault();
                 event.stopPropagation();
             }
@@ -198,10 +207,13 @@ Array.prototype.slice.call(forms)
                     let field = document.getElementsByName(fieldName);
                     //A regex pattern exists for the field
                     if (patterns[field[0].id]) {
-                        validate(field[0], patterns[field[0].id]);
+                        validate(field[0], patterns[field[0].id], true);
+                    } else if (field[0] === password) {
+                        //The password is always empty on the page reload --> no regex needed
+                        validate(field[0], "", true);
                     }
                 }
-                form.classList.add('was-validated');
+                //form.classList.add('was-validated');
             } else {
                 let myModal = document.getElementById('exampleModal');
                 //Check if the modal element exists
@@ -212,7 +224,6 @@ Array.prototype.slice.call(forms)
             }
         }
     });
-
 
 
 
