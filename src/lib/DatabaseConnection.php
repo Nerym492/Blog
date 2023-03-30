da1dcfcae973aba1e69de78a6bcf573fef8dcfe8
Florian Pohu <florianpohu49@gmail.com>
2023-03-30 10:24:07 +0200

    Added function to execute query with limit and offset

--- src/lib/DatabaseConnection.php
+++ src/lib/DatabaseConnection.php
@@ -20,7 +20,35 @@ class DatabaseConnection
                 $_SESSION['messageClass'] = "danger";
             }
         }
-
         return $this->database;
     }
+
+    public function execQueryWithLimit(int $nbRows, int $rowLimit, int $offset): array
+    {
+
+        $connexion = $this->getConnection();
+        $connexion->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+
+        $statement = $connexion->prepare(
+            "SELECT *
+                       FROM (
+                            SELECT p.post_id, p.user_id, p.title, p.excerpt, p.content, p.last_update_date, 
+                                   p.creation_date, u.pseudo
+                            from blog.post p
+                            LEFT OUTER JOIN blog.user u on p.user_id = u.user_id
+                            LIMIT :limitParam OFFSET :offsetParam
+                        ) p
+                    ORDER BY p.post_id DESC"
+        );
+        $statement->execute([
+            ':limitParam' => $rowLimit,
+            ':offsetParam' => $offset
+        ]);
+
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+
+        return $result;
+    }
+
 }
